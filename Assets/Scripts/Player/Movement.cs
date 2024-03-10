79071bd8d941124bd48f478d714dfd99da1e1963
Idkanycode <69693864+Danny112111@users.noreply.github.com>
2024-03-10 20:03:52 +0100

    upload of every data

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -14,6 +14,8 @@ public class Movement : MonoBehaviour
     public float speed = 8;
     public float gravity = 9.81f;
     public float jumpHeight = 1.5f;
+    public float dashTime;
+    public float dashCooldown;
     
 
     public Transform groundCheck;
@@ -36,15 +38,17 @@ public class Movement : MonoBehaviour
     void Update()
     {
         
-        
+        //creates little invisible sphere beneath the player to check for those parameters
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         isCeiling = Physics.CheckSphere(groundCheck.position, groundDistance, ceilingMask);
         gravityPad = Physics.CheckSphere(groundCheck.position, groundDistance, gravityPadMask);
         jumpPad = Physics.CheckSphere(groundCheck.position, groundDistance, jumpPadMask);
 
+        //this means, that if the player touches the "gravity pad" the CURRENT gravity will be inverted meaning that if you are upside down
+        //and touch it again you will be set to "normal gravity"
         bool gravityChange = gravityPad;
-        Debug.Log(gravity);
         
+        //those if statements reset the velocity brought by the physics with the player controller to not always accelerate
         if (isGrounded && velocity.y < 0 || isCeiling && velocity.y < -3f)
         {
             velocity.y = -2f;
@@ -54,16 +58,26 @@ public class Movement : MonoBehaviour
         { 
             velocity.y = 2f;
         }
-
+        
+        //caps the maximum velocity when falling 
         if (velocity.y > 20f)
         {
             velocity.y = 20f;
         }
-
+        
+        //as the name states, pushes you high in the air when the player touches the layer "JumpPad"
+        //when the player touches the layer, it turns the bool "jumpPad" true for enough time to execute the code beneath
         if (jumpPad)
         {
             velocity.y = 0;
             velocity.y += 13;
+        }
+        
+        //checks if gravity is inverted to let you use the jump pad on the ceiling
+        if (jumpPad && gravity > 0)
+        {
+            velocity.y = 0;
+            velocity.y -= 13;
         }
 
         float x = Input.GetAxis("Horizontal");
@@ -71,18 +85,19 @@ public class Movement : MonoBehaviour
 
         Vector3 move = transform.right * x + transform.forward * z;
         
-        
+        //lets you jump on the ground
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
         }
         
-        
+        //lets you jup when with inverted gravity on any (yet coded) layer
         if (Input.GetButtonDown("Jump") && isCeiling ||Input.GetButtonDown("Jump") && isGrounded && gravity > 0)
         {
             velocity.y = jumpHeight * gravity * -0.5f;
         }
-
+    
+        //inverts the gravity when bool is set to true
         if (gravityChange)
         {
             gravity = gravity * -1;
@@ -105,14 +120,18 @@ public class Movement : MonoBehaviour
     {
         
         bool dash = Input.GetKey(KeyCode.LeftShift);
-        
-        if (dash)
+
+        if (dash && dashCooldown > Time.time)
         {
             speed = 11;
+            dashTime += 1;
         }
         else if (!dash)
         {
             speed = 8;
         }
+        
+        
+
     }
 }

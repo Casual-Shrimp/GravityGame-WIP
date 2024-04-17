86c2b0d6e0dda5f4b2e6e3127fc266e70841c661
Idkanycode <69693864+Casual-Shrimp@users.noreply.github.com>
2024-04-17 22:56:02 +0200

    Finally got back
    I finally got back to my project. Deleted dash for now. Tried to fix a problem with jumping giving inconsistent height (kinda fixed it for only normal gravity). Next goal really fixing jumping and tryping to make it good, learn raycasting for interaction with environment, implement doors that can be opened with a button (later through puzzles)

--- Assets/Scripts/Player/Movement.cs
+++ Assets/Scripts/Player/Movement.cs
@@ -10,13 +10,12 @@ public class Movement : MonoBehaviour
 {
 
     public CharacterController controller;
-    
-    public float speed = 8;
-    public float gravity = 9.81f;
-    public float jumpHeight = 1.5f;
-    public float dashTime;
-    public float dashCooldown;
-    
+
+    public float speed = 10;
+    public float gravity = -9.81f;
+    public float jumpHeight = 1f;
+
+
 
     public Transform groundCheck;
     public float groundDistance = 0.1f;
@@ -24,20 +23,20 @@ public class Movement : MonoBehaviour
     public LayerMask ceilingMask;
     public LayerMask gravityPadMask;
     public LayerMask jumpPadMask;
-    
+
     public Vector3 velocity;
     public bool isGrounded;
     public bool isCeiling;
     public bool gravityPad;
     public bool jumpPad;
-    
-    
 
 
-    
+
+
+
     void Update()
     {
-        
+
         //creates little invisible sphere beneath the player to check for those parameters
         isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
         isCeiling = Physics.CheckSphere(groundCheck.position, groundDistance, ceilingMask);
@@ -47,24 +46,24 @@ public class Movement : MonoBehaviour
         //this means, that if the player touches the "gravity pad" the CURRENT gravity will be inverted meaning that if you are upside down
         //and touch it again you will be set to "normal gravity"
         bool gravityChange = gravityPad;
-        
-        //those if statements reset the velocity brought by the physics with the player controller to not always accelerate
-        if (isGrounded && velocity.y < 0 || isCeiling && velocity.y < -3f)
+
+        //those "if statements" reset the velocity brought by the physics with the player controller to not always accelerate
+        if (isGrounded && velocity.y < 0 || isCeiling && velocity.y < -3f && gravity < 0)
         {
             velocity.y = -2f;
         }
 
-        if (isCeiling && velocity.y > 0 || isGrounded && velocity.y > 0f)
-        { 
+        if (isCeiling && velocity.y > 0 || isGrounded && velocity.y > 0f && gravity > 0)
+        {
             velocity.y = 2f;
         }
-        
+
         //caps the maximum velocity when falling 
         if (velocity.y > 20f)
         {
             velocity.y = 20f;
         }
-        
+
         //as the name states, pushes you high in the air when the player touches the layer "JumpPad"
         //when the player touches the layer, it turns the bool "jumpPad" true for enough time to execute the code beneath
         if (jumpPad)
@@ -72,7 +71,7 @@ public class Movement : MonoBehaviour
             velocity.y = 0;
             velocity.y += 13;
         }
-        
+
         //checks if gravity is inverted to let you use the jump pad on the ceiling
         if (jumpPad && gravity > 0)
         {
@@ -84,19 +83,21 @@ public class Movement : MonoBehaviour
         float z = Input.GetAxis("Vertical");
 
         Vector3 move = transform.right * x + transform.forward * z;
-        
+
         //lets you jump on the ground
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            Debug.Log(velocity.y);
+            Debug.Log(gravity);
         }
-        
+
         //lets you jup when with inverted gravity on any (yet coded) layer
-        if (Input.GetButtonDown("Jump") && isCeiling ||Input.GetButtonDown("Jump") && isGrounded && gravity > 0)
+        if (Input.GetButtonDown("Jump") && isCeiling || Input.GetButtonDown("Jump") && isGrounded && gravity > 0)
         {
             velocity.y = jumpHeight * gravity * -0.5f;
         }
-    
+
         //inverts the gravity when bool is set to true
         if (gravityChange)
         {
@@ -104,34 +105,14 @@ public class Movement : MonoBehaviour
             velocity.y = velocity.y * -1;
             transform.Rotate(0, 0, 180);
         }
-        
-        
+
+
         controller.Move(move * speed * Time.deltaTime);
 
         velocity.y += gravity * Time.deltaTime;
 
         controller.Move(velocity * Time.deltaTime);
 
-        Dash();
-
-    }
-    
-    void Dash()
-    {
-        
-        bool dash = Input.GetKey(KeyCode.LeftShift);
-
-        if (dash && dashCooldown > Time.time)
-        {
-            speed = 11;
-            dashTime += 1;
-        }
-        else if (!dash)
-        {
-            speed = 8;
-        }
-        
-        
-
     }
 }
+    
